9eee2254b51b182a99ddbfd0def39ab4bc349f6d
Devikaponnieth <159915649+Devikaponnieth@users.noreply.github.com>
2024-02-20 16:18:23 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function showcollapse(){
+    document.getElementById("collapsediv").style.display="block";
+}
+
+function hidecollapse(){
+    document.getElementById("collapsediv").style.display="none";
+}
